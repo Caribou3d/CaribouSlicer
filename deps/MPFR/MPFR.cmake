5224c66a3692bcc4a71f42d7a11bca3067f36330
wschadow <wolfgang.schadow@t-online.de>
2025-05-27 00:11:12 +0200

    updated links in deps

--- deps/MPFR/MPFR.cmake
+++ deps/MPFR/MPFR.cmake
@@ -26,9 +26,9 @@ else ()
     endif ()
 
     ExternalProject_Add(dep_MPFR
-        URL https://www.mpfr.org/mpfr-current/mpfr-4.2.1.tar.bz2
+        URL https://www.mpfr.org/mpfr-4.2.1/mpfr-4.2.1.tar.bz2
         URL_HASH SHA256=b9df93635b20e4089c29623b19420c4ac848a1b29df1cfd59f26cab0d2666aa0
-        DOWNLOAD_DIR ${DEP_DOWNLOAD_DIR}/MPFR
+    DOWNLOAD_DIR ${DEP_DOWNLOAD_DIR}/MPFR
         BUILD_IN_SOURCE ON
         CONFIGURE_COMMAND env "CFLAGS=${_gmp_ccflags}" "CXXFLAGS=${_gmp_ccflags}" ./configure ${_cross_compile_arg} --prefix=${DESTDIR}/usr/local --enable-shared=no --enable-static=yes --with-gmp=${DESTDIR}/usr/local ${_gmp_build_tgt}
         BUILD_COMMAND make -j
